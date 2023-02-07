ff7ed9dcdf58249b94d4bdc659975c229fdedb76
Harry <fmjh@hotmail.com>
2023-02-06 22:16:13 -0300

    Adding a main menu

---
+++ Assets/Script/LogoScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoScript : MonoBehaviour
+{
+    private float deltaTime;
+    private float senoidalFrequency;
+    private float senoidalAmplitude;
+    private Quaternion rotationOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        senoidalFrequency = 0.8f;
+        senoidalAmplitude = 0.3f;
+        rotationOffset = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += Time.deltaTime;
+        transform.rotation = rotationOffset;
+        transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, rotationOffset.z + Mathf.Sin(senoidalFrequency * Mathf.PI * deltaTime) * senoidalAmplitude, transform.rotation.w);
+    }
+}
---
+++ Assets/Script/MainMenuScript.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    private Image transitionImage;
+    private float transitionSpeed;
+    private bool startTransition;
+
+    void Start()
+    {
+        transitionImage = GameObject.Find("Transition").GetComponent<Image>();
+        transitionImage.material.SetFloat("_Cutoff", 1);
+        transitionSpeed = 2f;
+        startTransition = false;
+    }
+
+    void Update()
+    {
+        if (startTransition)
+        {
+            transitionImage.material.SetFloat("_Cutoff", Mathf.MoveTowards(transitionImage.material.GetFloat("_Cutoff"), -0.1f - transitionImage.material.GetFloat("_Edge_Smoothing"), transitionSpeed * Time.deltaTime));
+
+            if (transitionImage.material.GetFloat("_Cutoff") == -0.6f)
+            {
+                SceneManager.LoadScene("Level 1");
+            }
+        }
+        else
+        {
+            transitionImage.material.SetFloat("_Cutoff", Mathf.MoveTowards(transitionImage.material.GetFloat("_Cutoff"), 1.1f, transitionSpeed * Time.deltaTime));
+        }
+
+    }
+
+    [ContextMenu("Transition")]
+    public void Transition()
+    {
+        startTransition = !startTransition;
+    }
+
+    public void StartGame()
+    {
+        startTransition = true;
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
